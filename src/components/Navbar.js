53ea9697f17605bc142338bebc67c90f74901aba
Cuthbert20 <knowltonspencer@gmail.com>
2019-07-18 12:02:54 -0600

    first push

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+    return(
+        <nav>
+            <p>Dogs A Comprehensive Guide to EVERY Breed of Dog on Planet Earth</p>
+            <a href=""><i class="fas fa-dog fa-lg"></i></a>
+           
+        </nav>
+    )
+}
---
+++ src/data.js
@@ -0,0 +1,26 @@
+const dogs = [
+  {
+    id: 1,  
+    breed: "bully",
+    img: "C:\Users\Spencer Knowlton\Documents\devMountain\personal-project\first-personal-project\first-personal-project\imgs\bully.jpg",
+    info: "Bully breeds all come from the same root stock called Molosser, which is a breed that originated in ancient Greece. Molossers were big dogs with large bones and muscles, pendant ears and short muzzles.",
+    cool: true
+  },
+  {
+    id: 2,  
+    breed: "goldendoodle",
+    img: "C:\Users\Spencer Knowlton\Documents\devMountain\personal-project\first-personal-project\first-personal-project\imgs\goldendoodle.jpeg",
+    info: "An adorable mix between the Golden Retriever and Poodle, Goldendoodles are the ultimate combination of good looks, smart wits, and playfulness. The hybrid is known as a “designer breed” because they were bred to have the lovable qualities of both of their parents.",
+    cool: true
+  },
+  {
+    id: 3,  
+    breed: "poodle",
+    img: "C:\Users\Spencer Knowlton\Documents\devMountain\personal-project\first-personal-project\first-personal-project\imgs\poodle.jpg",
+    info: "Poodles are a group of formal dog breeds, the Standard Poodle, Miniature Poodle and Toy Poodle. The origin of the breed is still discussed, with a prominent dispute over whether the poodle descends from Germany as a type of water dog, or from the French Barbet.",
+    cool: false
+  }
+  
+]
+
+module.exports = dogs
