ccb11ff945592cc0b8ac36873cb9c1ffd018d59b
Cuthbert20 <knowltonspencer@gmail.com>
2019-07-19 14:16:53 -0600

    just to be safe

--- src/components/AddBreed.js
+++ src/components/AddBreed.js
@@ -5,32 +5,45 @@ export default class AddBreed extends Component {
         super(props)
 
         this.state = {
-            userInput: ""
+            breed: "",
+            info: "",
+            img: ""
+
         }
-        this.handleChange = this.handleChange.bind(this)
+        this.handleBreedChange = this.handleBreedChange.bind(this)
+        this.handleInfoChange = this.handleInfoChange.bind(this)
+        this.handleImageChange = this.handleImageChange.bind(this)
     }
     //again you can't change props
-    handleChange(val){
-        this.setState({userInput: val})
+    handleBreedChange(val){
+        this.setState({breed: val})
+    }
+    handleInfoChange(val){
+        this.setState({info: val})
+    }
+    handleImageChange(val){
+        this.setState({img: val})
     }
             render(){
+                const { breed,info,img } = this.state
                 return(
                     //onchange this.setstate
                     <main>
                     <input placeholder="Breed" 
                     type="text"
-                    onChange={(e) => this.handleChange(e.target.value)} type="text" />
+                    onChange={(e) => this.handleBreedChange(e.target.value)} type="text" />
                     
                     <input placeholder="info" type="text"
-                    onChange={(e) => this.handleChange(e.target.value)} type="text" />
+                    onChange={(e) => this.handleInfoChange(e.target.value)} type="text" />
                     
                     <input placeholder="URL Img"type="text"
-                    onChange={(e) => this.handleChange(e.target.value)} type="text" />
+                    onChange={(e) => this.handleImageChange(e.target.value)} type="text" />
 
                     {/* using axios.post to update our breeds axios calls are only
                     called on buttons. You don't want to put it on an input
                     because everytime they type it will send the axios request */}
-                    <button onClick={() => this.props.addDog({breed: this.state.userInput})} >Submit</button>
+                    {/* <img src={img} alt=""/> */}
+                    <button onClick={() => this.props.addDog({breed: breed, info: info, img: img})} >Submit</button>
                     </main>
                 )
             }
